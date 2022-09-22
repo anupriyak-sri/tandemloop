0d22eca2d1bf9c9f927f08a8bd70b29fa62e2336
anu <anusri119@gmail.com>
2022-09-22 15:40:37 +0530

    program 1t0 4

---
+++ program1.js
@@ -0,0 +1,34 @@
+//calculator
+
+calc(5,10,"+")
+calc(10,5,"-")
+calc(10,5,"*")
+calc(10,5,"/")
+
+function calc(number1,number2,operation){
+    var result
+switch(operation) {
+    case '+':
+         result = number1 + number2;
+        console.log(`${number1} + ${number2} = ${result}`);
+        break;
+
+    case '-':
+         result = number1 - number2;
+        console.log(`${number1} - ${number2} = ${result}`);
+        break;
+
+    case '*':
+         result = number1 * number2;
+        console.log(`${number1} * ${number2} = ${result}`);
+        break;
+
+    case '/':
+         result = number1 / number2;
+        console.log(`${number1} / ${number2} = ${result}`);
+        break;
+
+    default:
+        console.log('Invalid operator');
+        break;
+}}
